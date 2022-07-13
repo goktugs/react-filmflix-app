3615d4b2880d25c28339743d74bb26dc6e0fc824
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-07-13 21:32:29 +0300

    Organise Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CssBaseline />
+      <main>
+        <Switch>
+          <Route path="/actors/:id" exact>
+            <h1>Actors</h1>
+          </Route>
+          <Route path="/movies/:id" exact>
+            <h1>Movies Information</h1>
+          </Route>
+          <Route path="/" exact>
+            <h1>Movies</h1>
+          </Route>
+          <Route path="/profile/:id" exact>
+            <h1>Profile</h1>
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
