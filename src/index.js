5c2449dd21d8bb29e8b357fd7eb4bd76f5a81ab3
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-07-14 18:49:51 +0300

    Setup Redux for Movies from TMDB Api

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import { Provider } from 'react-redux';
+import store from './stores';
 const theme = createTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/stores/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { tmdbApi } from '../services/TMBD';
+
+export default configureStore({
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+  },
+});
