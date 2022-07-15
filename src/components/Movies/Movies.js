f1ce0ea913eca703380883015d079652c5d9a37c
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-07-15 13:44:19 +0300

    Add Movies to Frontpage

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -6,13 +6,40 @@ import {
   Typography,
 } from '@mui/material';
 import { useSelector } from 'react-redux';
-
+import { MovieList } from '..';
 import { useGetMoviesQuery } from '../../services/TMBD';
 
 export default function Movies() {
-  const { data } = useGetMoviesQuery;
+  const { data, error, isFetching } = useGetMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies matched <br />
+          Please search something else!!
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) {
+    return 'Error accured';
+  }
 
   console.log(data);
 
-  return <div>Movies</div>;
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 }
